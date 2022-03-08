cc140985d9d21f30bebc4c3884bcb8823fa69815
N. Fajri <fajriajjha@gmail.com>
2022-03-08 17:31:44 +0700

    fix: log database name

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,5 @@
 import { logErrServer, logServer } from '@expresso/helpers/Formatter'
+import chalk from 'chalk'
 import mongoose from 'mongoose'
 import {
   MONGODB_AUTH,
@@ -22,16 +23,17 @@ const setOptions = {
 
 const initialMongoDB = (): void => {
   const msgTye = 'mongodb'
+  const dbName = chalk.cyan(MONGODB_DATABASE)
 
   // Connecting to the database
   mongoose
     .connect(setUri, setOptions)
     .then(() => {
-      const message = `Successfully connected to the database : ${MONGODB_DATABASE}`
+      const message = `Successfully connected to the database : ${dbName}`
       console.log(logServer(msgTye, message))
     })
     .catch((err) => {
-      const message = `Could not connect to the MongoDB database : ${MONGODB_DATABASE}`
+      const message = `Could not connect to the MongoDB database : ${dbName}`
       console.log(logErrServer(msgTye, message), err)
 
       process.exit()
